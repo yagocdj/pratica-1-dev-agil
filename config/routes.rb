e66d43a686d822b6c58edee2c50187f402c2b440
Yago César <yagocdj@gmail.com>
2024-12-04 15:00:56 -0300

    feat: criar index com as lojas

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :contacts
   resources :stores
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
